b42a1a55ea8f4621005c170785ad6c68111fe3a9
root <root@jx-staging-wd-flow-deployer-6855cc775f-rb5dj>
2023-12-11 19:32:57 +0000

    Flow Creation

---
+++ src/functions/99-99-be01b1491e58ecf1c80a.ts
@@ -0,0 +1,19 @@
+import { app, InvocationContext } from "@azure/functions";
+import * as https from "https";
+import * as df from 'durable-functions';
+import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
+
+const orchestrator = () => {}
+
+export async function serviceBusQueueTrigger(message: unknown, context: InvocationContext): Promise<void> {
+    context.log('Service bus queue function processed message:', message);
+    const client = df.getClient(context);
+    const instanceId: string = await client.startNew("orchestrator", message);
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+}
+app.serviceBusQueue('orchestrator', {
+    connection: 'azureQueueConnection',
+    queueName: '99-99-be01b1491e58ecf1c80a',
+    handler: serviceBusQueueTrigger,
+    extraInputs: [df.input.durableClient()],
+});
